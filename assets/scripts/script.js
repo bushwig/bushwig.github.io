9f94ad7a3af516e9809fdcb96ea94887a1ccb773
ian adams <ianadams@ians-MacBook-Pro-3.local>
2017-07-22 16:01:26 -0500

    parallax sidebars and topbar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
+    $(window).scroll(function(e){
+      parallax();
+    });
+
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.parallax').css('top',-(scrolled*0.15)+'px');
+    }
+
 });
