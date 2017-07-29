0e34bd7dcf18204b923aba8966cac2331af75a89
ian adams <ianadams@ians-MacBook-Pro-3.local>
2017-07-29 15:19:47 -0500

    add interior page content

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,4 +9,17 @@ $(document).ready(function() {
       $('.parallax').css('top',-(scrolled*0.15)+'px');
     }
 
+    var current = location.pathname;
+    $('#menu li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    });
+
+    if (current == '/info.html') {
+        $('#menu li img').attr('src','https://s3.amazonaws.com/ijadams/bushwig/lil-peace-bold.png');
+    }
+
 });
