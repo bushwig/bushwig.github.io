de2fb39b6c161dfa48b0ad1c0d7540fdeeb9e43a
Ian Adams <ian.adams@ge.com>
2018-02-07 11:05:38 -0600

    init commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,3 @@
 $(document).ready(function() {
 
-    $(window).scroll(function(e){
-      parallax();
-    });
-
-    function parallax(){
-      var scrolled = $(window).scrollTop();
-      $('.parallax').css('top',-(scrolled*0.15)+'px');
-    }
-
-    var current = location.pathname;
-    $('#menu li a').each(function(){
-        var $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) !== -1){
-            $this.addClass('active');
-        }
-    });
-
-    if (current == '/info.html') {
-        $('#menu li img').attr('src','https://s3.amazonaws.com/ijadams/bushwig/lil-peace-bold.png');
-    }
-
 });
